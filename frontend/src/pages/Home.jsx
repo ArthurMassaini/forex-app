077954eac9325585ee7e82cb5a658c99d4fe26b4
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-05-17 10:55:28 -0300

    Redirect if there is no user logged

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+import * as STORAGE from '../services/localStorage';
 
 function Home() {
+  if (STORAGE.getUser() === null) {
+    return <Redirect to="/login" />;
+  }
+
   return <main>Home</main>;
 }
 
