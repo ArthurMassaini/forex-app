ef31c3afc348f6039d5f6bfa59e2904239fcdc08
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-05-17 10:45:02 -0300

    Login and Home rendering

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
 // import Register from './pages/Register';
 
 const Routes = () => (
