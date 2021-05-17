dc457d40055e6070af7227162f92a7fc9124d833
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-05-17 10:42:11 -0300

    Set up routes with react-router-dom and create pages template of Home and Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+
+import Routes from './routes';
 import './App.css';
 
 function App() {
-  return <div className="App">App</div>;
+  return <Routes />;
 }
 
 export default App;
---
+++ frontend/src/routes/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+// import Register from './pages/Register';
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    {/* <Route exactpath="/register" component={Register} /> */}
+  </Switch>
+);
+
+export default Routes;
