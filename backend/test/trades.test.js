3e7566b5566257a861bb278d45d8689680f78463
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-05-25 07:55:40 -0300

    Fiinish test trades endpoint

--- backend/test/trades.test.js
+++ backend/test/trades.test.js
@@ -5,6 +5,106 @@ const mongoDbUrl = 'mongodb://localhost:27017/Forex';
 const dbName = 'Forex';
 const url = 'http://localhost:3001';
 
+describe('Create an endpoint for create a new trade', () => {
+  let db, connection;
+
+  // before all tests will have a connection to a test DB
+  beforeAll(async () => {
+    connection = await MongoClient.connect(mongoDbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = connection.db(dbName);
+  });
+
+  // after all tests will disconnect from the db and delete the test user
+  afterAll(async () => {
+    await db.collection('users').deleteMany({ name: 'PersonName LastName' });
+    await connection.close();
+  });
+
+  it('should be able to create a new trade', async () => {
+    let resultUser;
+
+    await frisby
+      .post(`${url}/users/`, {
+        name: 'PersonName LastName',
+        email: 'personname@gmail.com',
+        password: '123456',
+      })
+      .expect('status', 201);
+
+    await frisby
+      .post(`${url}/login/`, {
+        name: 'PersonName LastName',
+        email: 'personname@gmail.com',
+        password: '123456',
+      })
+      .expect('status', 200)
+      .then((response) => {
+        const { body } = response;
+        resultUser = JSON.parse(body);
+        return frisby
+          .setup({
+            request: {
+              headers: {
+                Authorization: resultUser.token,
+                'Content-Type': 'application/json',
+              },
+            },
+          })
+          .post(`${url}/trades`, {
+            high: 1.4,
+            low: 1.3,
+            datetime: '2021-05-20 10:09:00',
+            userId: resultUser.id,
+            quantity: 12000,
+            type: 'sell',
+          })
+          .expect('status', 201)
+          .then((responseTrade) => {
+            const { body } = responseTrade;
+            resultTrade = JSON.parse(body);
+            expect(resultTrade.trade.status).toBe('open');
+            expect(resultTrade.trade.userId).toBe(resultUser.id);
+            expect(resultTrade.trade.type).toBe('sell');
+            expect(resultTrade.message).toBe('Trade successfully placed!');
+          });
+      });
+  });
+  it('should not be able to create a trade with invalid entries', async () => {
+    let resultUser;
+
+    await frisby
+      .post(`${url}/login/`, {
+        name: 'PersonName LastName',
+        email: 'personname@gmail.com',
+        password: '123456',
+      })
+      .expect('status', 200)
+      .then((response) => {
+        const { body } = response;
+        resultUser = JSON.parse(body);
+        return frisby
+          .setup({
+            request: {
+              headers: {
+                Authorization: resultUser.token,
+                'Content-Type': 'application/json',
+              },
+            },
+          })
+          .post(`${url}/trades`, {})
+          .expect('status', 400)
+          .then((responseTrade) => {
+            const { body } = responseTrade;
+            resultTrade = JSON.parse(body);
+            expect(resultTrade.message).toBe('No entry can be undefined');
+          });
+      });
+  });
+});
+
 describe('Create an endpoint for get all trades by the UserID', () => {
   let db, connection;
 
@@ -66,7 +166,6 @@ describe('Create an endpoint for get all trades by the UserID', () => {
           .then((responseTrade) => {
             const { body } = responseTrade;
             resultTrade = JSON.parse(body);
-            console.log(resultTrade.trade.id);
           });
       });
 
@@ -211,7 +310,7 @@ describe('Create an endpoint for get all trades by the UserID', () => {
   });
 });
 
-describe('Create an endpoint for create a new trade', () => {
+describe('Create an endpoint for update a new trade', () => {
   let db, connection;
 
   // before all tests will have a connection to a test DB
@@ -229,6 +328,128 @@ describe('Create an endpoint for create a new trade', () => {
     await connection.close();
   });
 
-  it('should be able to create a new trade', () => {});
-  it('should not be able to create a trade with invalid entries', () => {});
+  it('should not be able to update a trade with invalid entries', async () => {
+    let resultTrade;
+    let resultUser;
+
+    await frisby
+      .post(`${url}/users/`, {
+        name: 'PersonName LastName',
+        email: 'personname@gmail.com',
+        password: '123456',
+      })
+      .expect('status', 201);
+
+    await frisby
+      .post(`${url}/login/`, {
+        name: 'PersonName LastName',
+        email: 'personname@gmail.com',
+        password: '123456',
+      })
+      .expect('status', 200)
+      .then((response) => {
+        const { body } = response;
+        resultUser = JSON.parse(body);
+        return frisby
+          .setup({
+            request: {
+              headers: {
+                Authorization: resultUser.token,
+                'Content-Type': 'application/json',
+              },
+            },
+          })
+          .post(`${url}/trades`, {
+            high: 1.4,
+            low: 1.3,
+            datetime: '2021-05-20 10:09:00',
+            userId: resultUser.id,
+            quantity: 1000,
+            type: 'buy',
+          })
+          .expect('status', 201)
+          .then((responseTrade) => {
+            const { body } = responseTrade;
+            resultTrade = JSON.parse(body);
+          });
+      });
+
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: resultUser.token,
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .put(`${url}/trades/${resultTrade.trade.id}`, {
+        userId: resultUser.id,
+      })
+      .expect('status', 400)
+      .then((response) => {
+        const { body } = response;
+        const result = JSON.parse(body);
+        expect(result.message).toBe('Invalid Entry');
+      });
+  });
+
+  it('should be able to update a trade successfully', async () => {
+    let resultTrade;
+    let resultUser;
+
+    await frisby
+      .post(`${url}/login/`, {
+        name: 'PersonName LastName',
+        email: 'personname@gmail.com',
+        password: '123456',
+      })
+      .expect('status', 200)
+      .then((response) => {
+        const { body } = response;
+        resultUser = JSON.parse(body);
+        return frisby
+          .setup({
+            request: {
+              headers: {
+                Authorization: resultUser.token,
+                'Content-Type': 'application/json',
+              },
+            },
+          })
+          .post(`${url}/trades`, {
+            high: 1.4,
+            low: 1.3,
+            datetime: '2021-05-20 10:09:00',
+            userId: resultUser.id,
+            quantity: 1000,
+            type: 'buy',
+          })
+          .expect('status', 201)
+          .then((responseTrade) => {
+            const { body } = responseTrade;
+            resultTrade = JSON.parse(body);
+          });
+      });
+
+    await frisby
+      .setup({
+        request: {
+          headers: {
+            Authorization: resultUser.token,
+            'Content-Type': 'application/json',
+          },
+        },
+      })
+      .put(`${url}/trades/${resultTrade.trade.id}`, {
+        profritOrLoss: 0.30131,
+        userId: resultUser.id,
+      })
+      .expect('status', 200)
+      .then((response) => {
+        const { body } = response;
+        const result = JSON.parse(body);
+        expect(result.message).toBe('Status successfully updated');
+      });
+  });
 });
